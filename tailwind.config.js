576725e4dc8b06c17e07be340557228e444a5397
Arthurdw <mail@arthurdw.com>
2021-04-13 19:48:53 +0200

    🌟 Fixed the problems.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,15 +4,11 @@ module.exports = {
   // purge: {
   //   mode: "all",
   //   enabled: true,
-  //   preserveHtmlElements: false,
   //   layers: ["base", "components", "utilities"],
-  //   content: [
-  //     "./pages/**/*.{js,ts,jsx,tsx}",
-  //     "./components/**/*.{js,ts,jsx,tsx}",
-  //   ],
+  //   content: ["./pages/**/*.{js,ts,jsx,tsx}"],
   //   options: {
   //     keyframes: true,
-  //     fontFace: true,
+  //     fontFace: true
   //   },
   // },
   darkMode: false, // or 'media' or 'class'
